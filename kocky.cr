fc6de8dc3c11108681ac5208204e664702ada191
Arthur <arthur.bernier@blavity.com>
2021-02-26 12:11:15 -0500

    Minor Changes
    Example of AST, Update Kocky.cr and jsx.mamba with voltron function

--- kocky.cr
+++ kocky.cr
@@ -1,8 +1,8 @@
 require "http/server"
-# 
+#
 server = HTTP::Server.new do |context|
   context.response.content_type = "text/html"
-  context.response.print "<h1>Hello World! This is Kofu and Crystal at #{Time.local}</h1>"
+  context.response.print "<h1>Hello World! This is Mamba and Crystal at #{Time.local}</h1>"
 end
 
 address = server.bind_tcp 8000
---
+++ msast.js
@@ -0,0 +1,108 @@
+{
+  "body": {
+    "statements": [{
+      "assignee": {
+        "data": "voltron",
+        "typeAnnotation": {
+          "nodeType": "functionType",
+          "returnType": {
+            "implicit": true,
+            "isPrimitive": true,
+            "nodeType": "primitiveIdentifier",
+            "identifier": {
+              "typeRef": "Any"
+            }
+          },
+          "arguments": [null]
+        }
+      },
+      "expression": {
+        "parameters": [{
+          "expression": {
+            "data": "args",
+            "typeAnnotation": null
+          }
+        }],
+        "body": {
+          "statements": [{
+            "function": {
+              "expression": {
+                "data": "Object",
+                "typeArguments": null,
+                "typeAnnotation": {
+                  "implicit": true,
+                  "isPrimitive": true,
+                  "nodeType": "primitiveIdentifier",
+                  "identifier": {
+                    "typeRef": "Any"
+                  }
+                }
+              },
+              "memberName": "assign",
+              "raw": null,
+              "typeAnnotation": {
+                "implicit": true,
+                "isPrimitive": true,
+                "nodeType": "primitiveIdentifier",
+                "identifier": {
+                  "typeRef": "Any"
+                }
+              }
+            },
+            "arguments": [{
+              "members": [],
+              "typeArguments": null,
+              "typeAnnotation": {
+                "properties": [],
+                "nodeType": "members",
+                "implicit": true,
+                "identifier": {
+                  "typeRef": "[object]"
+                }
+              }
+            }, {
+              "expression": {
+                "data": "args",
+                "typeArguments": null
+              }
+            }],
+            "raw": null,
+            "typeAnnotation": {
+              "implicit": true,
+              "isPrimitive": true,
+              "nodeType": "primitiveIdentifier",
+              "identifier": {
+                "typeRef": "Any"
+              }
+            }
+          }],
+          "typeAnnotation": {
+            "implicit": true,
+            "isPrimitive": true,
+            "nodeType": "primitiveIdentifier",
+            "identifier": {
+              "typeRef": "Any"
+            }
+          }
+        },
+        "typeAnnotation": {
+          "nodeType": "functionType",
+          "returnType": {
+            "implicit": true,
+            "isPrimitive": true,
+            "nodeType": "primitiveIdentifier",
+            "identifier": {
+              "typeRef": "Any"
+            }
+          },
+          "arguments": [null]
+        }
+      }
+    }]
+  },
+  "typeAnnotation": {
+    "identifier": {
+      "typeRef": "Program"
+    }
+  }
+}
